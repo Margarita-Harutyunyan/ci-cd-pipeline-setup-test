37282608305ea86b5045a656c8b8d7403252eb4f
Maga <magaharutyunyan969@gmail.com>
2024-10-28 13:08:26 +0400

    modified

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,21 @@
-// @ts-check
-
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import tseslint from "typescript-eslint";
+import eslint from "@eslint/js";
 
 export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+    eslint.configs.recommended,
+    ...tseslint.configs.strict,
+    ...tseslint.configs.stylisticTypeChecked,
+    {
+        rules: {
+            semi: ['error', 'always'],
+            quotes: ['error', 'single', { avoidEscape: true }],
+            indent: ['error', 2, { SwitchCase: 1 }],
+            "no-multiple-empty-lines": ["error", { "max": 1, "maxBOF": 0, "maxEOF": 0 }]
+        },
+        languageOptions: {
+            parserOptions: {
+                project: './tsconfig.json',
+            },
+        },
+    }
 );
